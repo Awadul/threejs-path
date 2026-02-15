1dfdcf756c0c41cdc1d26e01a2ade4d8ac007ad1
Muhammad Awais Abdullah <awaisabdullahm@gmail.com>
2026-02-15 17:53:21 +0500

    till the hero section model is being displayed with the loading text

--- threejs_portfolio/src/sections/Hero.jsx
+++ threejs_portfolio/src/sections/Hero.jsx
@@ -1,4 +1,10 @@
-import React from 'react'
+import {Canvas} from '@react-three/fiber'
+import { PerspectiveCamera } from '@react-three/drei'
+import HackerRoom from '../components/HackerRoom'
+import { Suspense } from 'react'
+import CanvasLoader from '../components/CanvasLoader'
+// import HackerRoom
+
 
 const Hero = () => {
   return (
@@ -8,7 +14,19 @@ const Hero = () => {
             <p className='hero_tag text-gray_gradient'>Realizing the unseen</p>
         </div>
 
-        <div className="w-full h-full absolute inset-0"></div>
+        <div className="w-full h-full absolute inset-0">
+            <Canvas className="w-full h-full">
+              <Suspense fallback={<CanvasLoader />} >
+
+                <PerspectiveCamera makeDefault position={[0,0,30]} />
+                <HackerRoom scale={0.07} position={[0,0,0]} rotation={[0, 280, 0]}/>
+
+                <ambientLight intensity={1} />
+                <directionalLight position={[10, 10, 10]} intensity={0.5}/>
+
+              </Suspense>
+            </Canvas>
+        </div>
     </section>
   )
 }
